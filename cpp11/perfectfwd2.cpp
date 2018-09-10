d4997b6fd0647a13a063e414438f949da8246d50
rk <kuro009900@gmail.com>
2018-09-10 23:09:43 +0900

    fix perfectfwd2

--- cpp11/perfectfwd2.cpp
+++ cpp11/perfectfwd2.cpp
@@ -3,52 +3,29 @@
 #include <memory>
 #include <iostream>
 #include <cassert>
+#include <type_traits>
 #include "boost/type_index.hpp"
 
 
-class MC1 {
-public:
-    template <typename T>
-    void func1(T x) {
-        std::cout << "func1(T)" << std::endl;
-//        return std::forward(x);
-    }
+class C1 {
 };
 
-template <typename T>
-void g(T&& x) {
-    std::cout << "type T : " << boost::typeindex::type_id_with_cvr<T&&>() << std::endl;
-    MC1<T&&> mc1{};
-    mc1.func1(std::forward<T&&>(x));
+template<typename T>
+void g1(T &&x) {
+    std::cout << "type T : " << boost::typeindex::type_id_with_cvr<T>() << std::endl;
+    static_assert(std::is_same<C1 &, T>::value, "");
+    static_assert(std::is_same<C1 &, decltype(x)>::value, "");
 }
 
-class C1 {
-public:
-    C1() : x_(1) {
-        std::cout << "C1()" << std::endl;
-    }
-    C1(const C1&) : x_(2) {
-        std::cout << "C1(const C1&)" << std::endl;
-    }
-    C1(C1&&) : x_(3) {
-        std::cout << "C1(C1&&)" << std::endl;
-    }
-    C1(int x) : x_(x) {
-        std::cout << "C1(int)" << std::endl;
-    }
-    virtual ~C1() {
-        std::cout << "~C1()" << std::endl;
-    }
-
-    int x() const {
-        return x_;
-    }
-private:
-    int x_;
-};
+template<typename T>
+void g2(T &&x) {
+    std::cout << "type T : " << boost::typeindex::type_id_with_cvr<T>() << std::endl;
+    static_assert(std::is_same<C1, T>::value, "");
+    static_assert(std::is_same<C1&&, decltype(x)>::value, "");
+}
 
 int main() {
-    C1 c1{-1};
-    g(c1);
-    g(C1{-2});
+    C1 c1{};
+    g1(c1);
+    g2(C1{});
 }
