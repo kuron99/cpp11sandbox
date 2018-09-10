828d458b7ced69a0723377ac787afcadaab6c8ae
Ryoji Kurosawa <kuro009900@gmail.com>
2018-09-10 16:32:31 +0900

    fix perfectfwd

--- cpp11/perfectfwd.cpp
+++ cpp11/perfectfwd.cpp
@@ -1,24 +1,32 @@
 #include <utility>
 #include <memory>
 #include <iostream>
+#include <cassert>
 
 class C1 {
 
 };
 
+static int xxx = 0;
 template <typename T>
 void func1(T&& x) {
     std::cout << "func1(T&&)" << std::endl;
+    xxx = 1;
 }
 
 void func1(std::unique_ptr<C1> ptr) {
     std::cout << "func1(unique_ptr)" << std::endl;
+    xxx = 2;
 }
 int main() {
     C1 c1{};
     std::unique_ptr<C1> ptr{new C1};
 
     func1(c1);
+    assert(xxx == 1);
     func1(std::move(ptr));
-
+    assert(xxx == 2);
+    xxx = 0;
+    func1(std::move(c1));
+    assert(xxx == 1);
 }
