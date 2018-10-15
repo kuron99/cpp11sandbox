154b2be618eb68c35fb86826de4e07eb97deca14
Ryoji Kurosawa <kuro009900@gmail.com>
2018-10-15 18:55:21 +0900

    fix test watch

--- cpp11/optimize/test_watch.cpp
+++ cpp11/optimize/test_watch.cpp
@@ -6,18 +6,22 @@ using namespace std;
 
 int main() {
     watch w1;
+    watch w2;
     vector<long long> v{};
-    v.reserve
+    v.reserve(100000000);
     try {
-        for (int j=0 ; j < 100; j++) {
-            w1.reset();
-            for (size_t i = 0; i < 10000000; i++) {
-
+        for (int j = 0; j < 100; j++) {
+            w2.reset();
+            const static long long len = 10000000;
+            for (size_t i = 0; i < len; i++) {
+                w1.reset();
+                v.push_back(w1.stop());
             }
-            cout << w1.stop()/1000 << "," << s.capacity() << endl;
+
+            const long long int dur = w2.stop();
+            cout << "j" << j << "duration: " << dur << " per loop: " << (double) dur / len << "microsec." << endl;
         }
-    } catch (std::bad_alloc& ) {
+    } catch (std::bad_alloc&) {
         cout << "std::bad_alloc exception caught" << endl;
     }
-    cout << "finish" << endl;
 }
