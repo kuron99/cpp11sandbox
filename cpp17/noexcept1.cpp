0de9a0d51c7d7c4838ed26806deac9fe4b343788
Ryoji Kurosawa <kuro009900@gmail.com>
2019-04-10 11:47:08 +0900

    add noexcept

---
+++ cpp17/noexcept1.cpp
@@ -0,0 +1,47 @@
+// you can use noexcept() operator to know if given operation raises exception
+// special generated functions ctor/dtor are noexcept by default unless base/member are potentially throwing on init
+
+#include <iostream>
+#include <bits/unique_ptr.h>
+
+class C1 {
+public:
+    C1() = default;
+    C1(int x) {}
+};
+
+void function_throws() {
+
+}
+void function_noexcept() noexcept {
+
+}
+
+void function_throws_with_noexcept() noexcept {
+    throw std::exception();
+}
+
+class C2 {
+public:
+    C2() = default;
+
+private:
+    C1 c1{1};
+};
+int main() {
+    static_assert(!noexcept(std::make_unique<C1>()));
+    static_assert(!noexcept(new C1));  // can throw bad alloc
+    static_assert(!noexcept(new C2));  // can throw bad alloc
+
+    // generated special function is noexcept if base/member doesn't throw on init
+    static_assert(noexcept(C1()));
+    static_assert(!noexcept(C1(1)));
+    static_assert(!noexcept(C2()));
+
+    static_assert(!noexcept(function_throws()));
+    static_assert(noexcept(function_noexcept()));
+    static_assert(noexcept(function_throws_with_noexcept()));
+
+    return 0;
+}
+
