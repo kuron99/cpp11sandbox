d391a8b690326de8260abb5de91f505b5263c8f5
Ryoji Kurosawa <kuro009900@gmail.com>
2019-05-28 19:39:55 +0900

    update dynamic_cast

--- cpp17/dynamic_cast1.cpp
+++ cpp17/dynamic_cast1.cpp
@@ -11,8 +11,8 @@
 class A { public: virtual ~A() {};};
 class V { public: virtual ~V() {}; std::int64_t z_{};};
 class C : public A, public virtual V { public: virtual ~C() {};std::int64_t x_{}; };
-class E : public C { public: virtual ~E() {};std::int64_t x_{}; };
 class D : public C { public: virtual ~D() {};std::int64_t x_{}; };
+class E : public C { public: virtual ~E() {};std::int64_t x_{}; };
 class F : public D, public E { public: virtual ~F() {};std::int64_t x_{}; };
 
 static_assert(sizeof(A) == 8);  // pointer to vtable
@@ -30,5 +30,37 @@ int main() {
     assert(a_ptr == (void*)&a);
     assert(nullptr != dynamic_cast<void*>(&a));
     F f{};
-    assert(nullptr != dynamic_cast<C*>(static_cast<D*>(&f)));
+    {
+        auto d = static_cast<D*>(&f);
+        auto e = dynamic_cast<E*>(d);
+        assert(e != nullptr);
+//    auto c = static_cast<C*>(&f); // F cannot be cast to C, that is ambiguous
+//        auto d2 = static_cast<D*>(e); // cross cast not allowed by static_cast
+        e->x_ = 100;
+        auto d2 = static_cast<D*>(static_cast<A*>(e)); // no compile error, but this is ill-formed
+        assert(d2 != nullptr);
+        assert(d2->x_ == 100);
+
+        auto c1 = static_cast<C*>(static_cast<E*>(&f)); // castable to C through non-ambiguous class
+        auto c2 = static_cast<C*>(static_cast<D*>(&f));
+        assert(reinterpret_cast<void*>(c1) != reinterpret_cast<void*>(c2));
+        auto f1 = dynamic_cast<F*>(c1);
+        auto f2 = dynamic_cast<F*>(c2);
+        assert(f1 == f2);
+        auto a1 = dynamic_cast<A*>(c1);
+        auto a2 = dynamic_cast<A*>(c2);
+        assert(a1 != a2);
+        auto v1 = dynamic_cast<V*>(c1);
+        auto v2 = dynamic_cast<V*>(c2);
+        assert(v1 == v2);
+    }
+
+    E e{};
+    {
+        auto c = static_cast<C*>(&e);
+        assert(nullptr != c);
+        auto d = dynamic_cast<D*>(c);
+        assert(nullptr == d);
+    }
+
 }
