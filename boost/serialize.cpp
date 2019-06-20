3fddace50c2e9713194d952b55401bfd332fa12f
Ryoji Kurosawa <kuro009900@gmail.com>
2019-06-20 16:29:09 +0900

    add serialize

---
+++ boost/serialize.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <vector>
+#include "boost/serialization/serialization.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/serialization/vector.hpp"
+
+using namespace boost::serialization;
+
+class C1 {
+//    friend boost::serialization::access;
+public:
+    C1() = default;
+    C1(int x, int y) : x_(x), y_(y) {}
+
+    template <class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        ar & x_;
+        ar & y_;
+    }
+    int x() {return x_;}
+    int y() {return y_;}
+
+private:
+    int x_;
+    int y_;
+};
+
+int main() {
+    std::vector<C1> vector;
+    std::ofstream ofs("testfile.dat");
+
+    vector.emplace_back(1,100);
+    vector.emplace_back(2,200);
+    vector.emplace_back(3,300);
+
+    {
+        boost::archive::binary_oarchive oa(ofs);
+        oa << vector;
+    }
+    std::vector<C1> restored;
+    {
+       std::ifstream ifs("testfile.dat");
+       boost::archive::binary_iarchive ia(ifs);
+       ia >> restored;
+    }
+    assert(3 == restored.size());
+    assert(100 == restored[0].y());
+    assert(200 == restored[1].y());
+    assert(300 == restored[2].y());
+}
