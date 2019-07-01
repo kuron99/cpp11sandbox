8dfc213b00f3560c42373416c0c8c5a7d6665a4e
Ryoji Kurosawa <kuro009900@gmail.com>
2019-07-01 20:00:07 +0900

    fix template parameter1

--- cpp17/template_parameter1.cpp
+++ cpp17/template_parameter1.cpp
@@ -3,6 +3,9 @@
 
 #include <vector>
 
+class C0 {
+
+};
 template <int N>
 class C1 {
 public:
@@ -13,7 +16,7 @@ template <template<int N> typename C, int M>
 class C2 : public C<M> {
 };
 
-template <template<int N> typename C, int M>
+template <template<int N = 10> typename C, int M>
 class C3 {
     std::vector<C<M>> vector_{};
 public:
@@ -23,6 +26,15 @@ public:
     }
 };
 
+// you can parameterize a type by using
+template <int N>
+using CX = C0;
+
+template<template<class, class> class Vector, class T, class Alloc = std::allocator<T>>
+class C4 {
+    Vector<T, Alloc> v;
+};
+
 int main() {
     // C1's parameter can be passed as one for C2
     static_assert(sizeof(C2<C1, 5>) == 5);
@@ -30,7 +42,10 @@ int main() {
 
     C1<5> c1{};
     C3<C1, 5> test;
+    C3<CX, 5> test2;
     test.add(c1);
+    C4<std::vector, C0, std::allocator<C0>> c4;
+    C4<std::vector, C0> c4_2{};
 
     return 0;
 }
