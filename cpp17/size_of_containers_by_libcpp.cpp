5136ae829c6607a6c62645b1406a354f380b6bbb
Ryoji Kurosawa <kuro009900@gmail.com>
2019-08-05 17:59:17 +0900

    add size of container for libc++

--- cpp17/size_of_containers_by_libcpp.cpp
+++ cpp17/size_of_containers_by_libcpp.cpp
@@ -1,5 +1,5 @@
 #include <ciso646>
-#ifndef _LIBCPP_VERSION
+#ifdef _LIBCPP_VERSION
 
 #include <any>
 #include <memory>
@@ -10,8 +10,8 @@
 #include <string_view>
 #include <iostream>
 #include <variant>
-#include <bits/stdc++.h>
 #include <boost/multiprecision/cpp_int.hpp>
+
 
 int main() {
     static_assert(sizeof(std::vector<int32_t>) == 24); // 3 ptrs (start, finish, end_of_stg)
@@ -22,7 +22,7 @@ int main() {
     static_assert(sizeof(std::array<int32_t, 6>) == 24);
     static_assert(sizeof(std::unique_ptr<int32_t>) == 8);
     static_assert(sizeof(std::shared_ptr<int32_t>) == 16); // ptr + ptr to control block
-    static_assert(sizeof(std::string) == 32); // SSO (16 char buffer + data length + data ptr(not used?)), non-SSO (capacity(16bytes) + data length + data ptr)
+    static_assert(sizeof(std::string) == 24); // SSO (16 char buffer + data length + data ptr(not used?)), non-SSO (capacity(16bytes) + data length + data ptr)
     static_assert(alignof(std::string) == 8);
     static_assert(sizeof(std::string_view) == 16); // ptr + len
     static_assert(sizeof(std::optional<int16_t>) == 4); // int16 + bool
@@ -34,7 +34,7 @@ int main() {
     static_assert(sizeof(std::optional<__int128>) == 32); // int128 + bool
     static_assert(alignof(std::optional<__int128>) == 16);
     static_assert(sizeof(__int128) == 16);
-    static_assert(sizeof(std::optional<std::string>) == 40); // string + bool
+    static_assert(sizeof(std::optional<std::string>) == 32); // string + bool
     static_assert(sizeof(std::optional<std::string_view>) == 24); // string_view + bool
     static_assert(sizeof(std::tuple<int32_t>) == 4);
     static_assert(sizeof(std::tuple<int64_t>) == 8);
@@ -42,20 +42,20 @@ int main() {
     static_assert(sizeof(std::tuple<int64_t, int64_t, int64_t>) == 24);
     static_assert(alignof(std::tuple<int64_t, int64_t, int64_t>) == 8);
 
-    static_assert(sizeof(std::any) == 16); // SSO (data 8 bytes + ptr to manager) or non-SSO (ptr to data + ptr to manager)
+    static_assert(sizeof(std::any) == 32); // SSO (data 8 bytes + ptr to manager) or non-SSO (ptr to data + ptr to manager)
     static_assert(alignof(std::any) == 8);
     static_assert(sizeof(std::variant<int32_t>) == 8);
     static_assert(sizeof(std::variant<int32_t, int64_t>) == 16);
     static_assert(sizeof(std::variant<int32_t, int64_t, float>) == 16);
     static_assert(sizeof(std::variant<int32_t, int64_t, float, double>) == 16);
     static_assert(sizeof(std::variant<int64_t>) == 16);
-    static_assert(sizeof(std::variant<std::string>) == 40);
-    static_assert(sizeof(std::variant<int64_t, std::string>) == 40);
-    static_assert(sizeof(std::function<std::int64_t(std::int64_t)>) == 32); // ptr to function, invoker, manager + ?
-    static_assert(alignof(std::function<std::int64_t(std::int64_t)>) == 8);
+    static_assert(sizeof(std::variant<std::string>) == 32);
+    static_assert(sizeof(std::variant<int64_t, std::string>) == 32);
+    static_assert(sizeof(std::function<std::int64_t(std::int64_t)>) == 48); // ptr to function, invoker, manager + ?
+    static_assert(alignof(std::function<std::int64_t(std::int64_t)>) == 16);
 
     static_assert(sizeof(void*) == 8);
     static_assert(sizeof(char[6]) == 6);
 }
 
-#endif // ifndef ciso646d
+#endif // _LIBCPP_VERSION
