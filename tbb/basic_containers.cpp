389fdfd068e59e60c08d7c4688b52627eafb469a
Ryoji Kurosawa <kuro009900@gmail.com>
2019-09-27 13:39:08 +0900

    basic containers

--- tbb/basic_containers.cpp
+++ tbb/basic_containers.cpp
@@ -2,11 +2,13 @@
 
 #include <thread>
 #include "tbb/concurrent_queue.h"
+#include "tbb/concurrent_priority_queue.h"
 
 int main() {
     tbb::concurrent_queue<int> cq{};
     tbb::concurrent_bounded_queue<int> cbq{};
 
+    tbb::concurrent_priority_queue<int> cpq{};
     int a;
     cbq.try_pop(a);
     cbq.pop(a);
