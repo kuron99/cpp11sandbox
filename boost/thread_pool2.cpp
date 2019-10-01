52d5a40f226a6425bf593cf6f2c3abe20b05ca74
Ryoji Kurosawa <kuro009900@gmail.com>
2019-10-01 11:06:30 +0900

    fix thread pool

--- boost/thread_pool2.cpp
+++ boost/thread_pool2.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <mutex>
 #include <future>
+#include <cassert>
 
 #include "thread_pool.h"
 
 using namespace std::chrono_literals;
 
-void freeze() {
-    std::cout << "th: " << std::this_thread::get_id() << std::endl;
-    std::this_thread::sleep_for(1h);
-}
 void f() {
     std::cout << "th: " << std::this_thread::get_id() << std::endl;
 }
@@ -19,13 +16,9 @@ int main() {
         std::cout << "packaged task " << std::endl;
         return 1;
     }};
-//    task();
     auto future = task.get_future();
-    pool.submit(task);
     pool.submit(std::ref(task));
     std::this_thread::sleep_for(1s);
-    std::cout << future.get() << std::endl;
+    assert(1 == future.get());
     pool.submit(f);
-//    pool.submit(f);
-//    pool.submit(f);
 }
