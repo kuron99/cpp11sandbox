65015e29f9ad0126116987d186759bf924e8e0b6
Ryoji Kurosawa <kuro009900@gmail.com>
2019-10-03 17:30:13 +0900

    extract_node

--- cpp17/extract_node.cpp
+++ cpp17/extract_node.cpp
@@ -20,19 +20,36 @@ public:
 int C1::counter_ = 0;
 
 int main() {
-    map<string, C1> map{};
-    map["A"s].x_ = 1;
-    map["B"s].x_ = 2;
-    assert(2 ==  C1::counter_);
-
-    assert(2 ==  map.size());
     {
-        auto node = map.extract("A"s);
-        assert("A"s == node.key());
-        assert(1 == node.mapped().x_);
-        assert(1 ==  map.size()); // after extract(), the element is removed from the map
+        map<string, C1> map{};
+        map["A"s].x_ = 1;
+        map["B"s].x_ = 2;
         assert(2 ==  C1::counter_);
+
+        assert(2 ==  map.size());
+        {
+            auto node = map.extract("A"s);
+            assert("A"s == node.key());
+            assert(1 == node.mapped().x_);
+            assert(1 ==  map.size()); // after extract(), the element is removed from the map
+            assert(2 ==  C1::counter_);
+        }
+        assert(1 ==  map.size()); // after destruction of node handle, the element is gone
+        assert(1 ==  C1::counter_);
     }
-    assert(1 ==  map.size()); // after destruction of node handle, the element is gone
-    assert(1 ==  C1::counter_);
+    {
+        map<string, C1> map1{};
+        map<string, C1> map2{};
+        map1["A"s].x_ = 1;
+        auto* c1 = &map1["A"s];
+        assert(1 == c1->x_);
+        {
+            auto node = map1.extract("A"s);
+            assert(1 == c1->x_); // pointer can be used after extract
+            assert("A"s == node.key());
+            assert(1 == c1->x_);
+            map2.insert(std::move(node));
+        }
+        assert(1 == c1->x_); // pointer can be used after extract/
+    }
 }
