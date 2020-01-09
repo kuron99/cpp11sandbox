797cdf0ba35909f85c10deafb63ed1a746de06ab
Ryoji Kurosawa <kuro009900@gmail.com>
2020-01-09 16:46:29 +0900

    hardware_destructive_interference_size

---
+++ cpp17/hardware_destructive_interference_size.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <new>
+
+int main() {
+    //not yet supported by gcc10 or clang 10
+//    std::cout << "hardware_destructive_interference_size: " << std::hardware_destructive_interference_size << std::endl;
+//    std::cout << "hardware_constructive_interference_size: " << std::hardware_constructive_interference_size << std::endl;
+
+}
