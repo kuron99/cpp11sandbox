fd080627c773e4dd06c220c9ab2501ecb31de920
Ryoji Kurosawa <kuro009900@gmail.com>
2020-03-26 12:56:21 +0900

    add more to using1.cpp

--- cpp17/using1.cpp
+++ cpp17/using1.cpp
@@ -1,4 +1,7 @@
 // typedef/using respects public/protected/private specifier
+// though function using private type can be accessed from client
+
+#include <cassert>
 
 class C1 {
 public:
@@ -10,13 +13,27 @@ protected:
 private:
     using mytype3 = int;
     typedef int t3;
+public:
+    void set_mytype3(mytype3 x) {
+        x_ = x;
+    }
+    mytype3 get_mytype3() {
+        return x_;
+    }
+private:
+    mytype3 x_{};
 };
 
 int main() {
     C1::mytype1 a;
     C1::t1 aa;
+
 //    C1::mytype2 b;
 //    C1::t2 bb;
 //    C1::mytype3 c;
 //    C1::t3 cc;
+
+    C1 c{};
+    c.set_mytype3(100); // client doesn't know mytype3 is int, but calling is ok
+    assert(100 == c.get_mytype3());
 }
