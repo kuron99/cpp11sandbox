6dbb05f62727b8e51cb60d1fae765888edec98ba
Ryoji Kurosawa <kuro009900@gmail.com>
2020-08-24 11:52:09 +0900

    over alignment

--- cpp17/over_alignment.cpp
+++ cpp17/over_alignment.cpp
@@ -1,12 +1,28 @@
 // gcc allows very large over alignment
 
 #include <cstddef>
+#include <cassert>
 #include <iostream>
+#include <vector>
+
+class alignas(65536) C1 {
+    int x;
+};
+
+class C2 {
+    int x;
+};
 
 int main() {
     static_assert(alignof(std::max_align_t) == 16);
-    alignas(65536) struct S { int c1; } s1;
+    C1 c;
+    std::cout << std::hex << &c << std::endl;
+    assert((std::ptrdiff_t (&c) & 65535UL) == 0);
 
-    std::cout << std::hex << &s1 << std::endl;
+    std::vector<C1> v(2);
+    assert((std::ptrdiff_t (&v[0]) & 65535UL) == 0);
+    assert((std::ptrdiff_t (&v[1]) & 65535UL) == 0);
+
+
 
 }
